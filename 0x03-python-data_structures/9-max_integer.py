84553362458c18ce62171667498b8059e200bb09
Kevin odo <111025280+Mattklevin@users.noreply.github.com>
2022-12-07 02:35:24 +0100

    Data

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-def multiple_returns(sentence):
-    length = len(sentence)
-    if length == 0:
-        first_char = None
-    else:
-        first_char = sentence[0]
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
 
-    new_tuple = (length, first_char)
-    return new_tuple
+    max_num = my_list[0]
+    for i in range(0, len(my_list)):
+        if my_list[i] > max_num:
+            max_num = my_list[i]
+    return max_num
