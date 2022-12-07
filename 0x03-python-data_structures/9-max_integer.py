edea26daa89cf06ce897ffe06002b46afc49d42d
Kevin odo <111025280+Mattklevin@users.noreply.github.com>
2022-12-07 02:34:08 +0100

    Data

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    length = len(sentence)
+    if length == 0:
+        first_char = None
+    else:
+        first_char = sentence[0]
+
+    new_tuple = (length, first_char)
+    return new_tuple
