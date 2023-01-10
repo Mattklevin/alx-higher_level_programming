b32e3a734895a044e5781b4ccf32a30bed044ded
Kevin odo <111025280+Mattklevin@users.noreply.github.com>
2023-01-10 05:02:30 +0100

    Update

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""intro to inheritance"""
+
+
+def lookup(obj):
+    """
+    uses the inbuilt dir() method to return
+    list of attributes and methods of an object
+    """
+    return dir(obj)
