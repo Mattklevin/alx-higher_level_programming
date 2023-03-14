2495c26d638af75421d60559a5090937df0f4b82
Kevin odo <111025280+Mattklevin@users.noreply.github.com>
2023-03-15 00:42:56 +0100

    Update

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
-// Script imports a dictionary of occurrences by user id
-// and computes a dictionary of user ids by occurrence.
-
-const { dict } = require('./101-data.js');
-const Dictn = {};
-for (const N in dict) {
-    if (Dictn[dict[N]] === undefined) {
-	Dictn[dict[N]] = [];
-    }
-    Dictn[dict[N]].push(N);
+const dict = require('./101-data.js').dict;
+let newDict = {};
+for (let key in dict) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [key];
+  } else {
+    newDict[dict[key]].push(key);
+  }
 }
-console.log(Dictn);
+console.log(newDict);
